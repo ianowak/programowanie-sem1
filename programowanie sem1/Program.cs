603602657a8ac5768d397e77f04e2e34bc9e4c91
ianowak <ingaannanowak@gmail.com>
2020-04-14 23:27:25 +0200

    Add project files.

---
+++ programowanie sem1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programowanie_sem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
